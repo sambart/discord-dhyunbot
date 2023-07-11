aaed8c9b6ed1d0ba775f4c14129f43fe3a478215
sambart <sambart17@gmail.com>
2023-07-11 14:19:33 +0900

    first

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import discord
+import os
 from discord import app_commands
 from discord.ext import commands
+import re
+
+#설치파일 확인
+try:
+    import nacl
+except ImportError:
+    try:
+        if os.name == 'nt':
+            os.system("py -m pip install pynacl")
+        else:
+            os.system("python3 -m install pynacl")
+    except Exception as e:
+        print("Error:", e)
+        exit()
 
 MY_GUILD = discord.Object(id=996054821100593273)
 TOKEN = 'MTAzOTA3NTE2MjUzODExOTE3OA.GPQ9z5.Go7CZTYVotNoouj4oCwMlGmixvD6BkVlrTkM9k'
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -17,26 +33,40 @@ async def on_ready():
         print(f"synced {len(synced)} command(s)")
     except Exception as e:
         print(e)
-        
-class MemberRoles(commands.MemberConverter):
-    async def convert(self, ctx, argument):
-        member = await super().convert(ctx, argument)
-        return [role.name for role in member.roles[1:]] # Remove everyone role!
 
-@bot.tree.command(name='say')
-@app_commands.describe(things_to_say = "뭐")
-async def say(interaction: discord.Interaction, things_to_say: str):
-    await interaction.response.send_message(f'{interaction.user.name} said: `{things_to_say}`')
+@bot.command(name='테스트1')
+async def say(ctx):
+    channel = ctx.author.voice.channel
 
-@app_commands.context_menu()
-async def react(interaction: discord.Interaction, message: discord.Message):
-    await interaction.response.send_message('Very cool message!', ephemeral=True)
+    for guild in bot.guilds:
+        for member in guild.members:
+            print({member})
+
+    await ctx.send(f"command test: {channel}")
+    await channel.connect()
+    
+    
+@bot.command(name='온앤오프닉정규화')  # 명령어 이름, 설명
+async def say(ctx):
+    embed = discord.Embed(title='title', description='description', url='', color=discord.Color.random())
+
+
+    p = re.compile('[abc]')
+    
+    for guild in bot.guilds:
+        for member in guild.members:
+            print({member})
+
+            #strMemeber += member.nick
+            embed.add_field(name='닉', value=member.nick, inline=True)
+    await ctx.send(embed=embed)
 
     
-@bot.tree.command(name='권한추가', description='testing')  # 명령어 이름, 설명
+@bot.tree.command(name='권한테스트', description='testing')  # 명령어 이름, 설명
+#@app_commands.describe()
 async def giverole(ctx, user: discord.Member, role: discord.Role):
+    #const guild = client.guilds.cache.get('Guild ID') 
     await user.add_roles(role)
-    await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving a role called: {role.name}")
 
 @bot.command(name="helpt", description="Returns all commands available")
 async def help(ctx):
